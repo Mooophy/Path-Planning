cc5d0e6091ce26632ff0f1d825cc28b7b5f19e0a
moophy <MoophyAlan@gmail.com>
2015-09-07 16:36:19 +1200

    tested goes

--- planning/UnitTests/test_node.cpp
+++ planning/UnitTests/test_node.cpp
@@ -11,13 +11,21 @@ namespace UnitTests
     {
     public:
 
+        //  0,0     0,1     0,2     ||  1   2   3
+        //  1,0     1,1     1,2     ||  4       5
+        //  2,0     2,1     2,2     ||  6   7   8
         TEST_METHOD(goes)
         {
             auto const c = Node::Coordinate{ 1u, 1u };
+
             Assert::IsTrue(Node::Coordinate{ 0u, 0u } == Node::goes.at('1')(c));
-            //
-            //  not finish yet
-            //
+            Assert::IsTrue(Node::Coordinate{ 0u, 1u } == Node::goes.at('2')(c));
+            Assert::IsTrue(Node::Coordinate{ 0u, 2u } == Node::goes.at('3')(c));
+            Assert::IsTrue(Node::Coordinate{ 1u, 0u } == Node::goes.at('4')(c));
+            Assert::IsTrue(Node::Coordinate{ 1u, 2u } == Node::goes.at('5')(c));
+            Assert::IsTrue(Node::Coordinate{ 2u, 0u } == Node::goes.at('6')(c));
+            Assert::IsTrue(Node::Coordinate{ 2u, 1u } == Node::goes.at('7')(c));
+            Assert::IsTrue(Node::Coordinate{ 2u, 2u } == Node::goes.at('8')(c));
         }
 
         TEST_METHOD(node_ctor)
