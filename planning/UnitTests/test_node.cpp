dd10b010fb4fe109b3588289c48900db8e8a3bde
moophy <MoophyAlan@gmail.com>
2015-09-07 12:22:43 +1200

    setup unit tests

---
+++ planning/UnitTests/test_node.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../lib/node.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
