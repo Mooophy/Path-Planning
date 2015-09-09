5f03222dd09d7365fe296662c65d163627cc2119
moophy <MoophyAlan@gmail.com>
2015-09-10 03:22:22 +1200

    setup skeleton for astar 	modified:   planning/lib/a_star_sel.hpp

--- planning/lib/a_star_sel.hpp
+++ planning/lib/a_star_sel.hpp
@@ -15,25 +15,29 @@ using std::string;
 
 namespace search
 {
-    template<typename Hfunc, typename Cfunc>
+    template<typename Hfunc, typename Cfunc, typename Validate>
     class AStarSEL
     {
     public:
+
         AStarSEL()
-            : _q{},
-            _max_q_size{ 0u },
-            _expansion{},
-            _final_path{ "" },
-            _run_time{ 0 }
+            : _q{}, _max_q_size{ 0u }, _expansion{}, _final_path{}, _run_time{ 0 }, _is_found{ false }
         {   }
 
     private:
 
         using Q = PriorityQueue<Node, Less<Node, Hfunc>>;
+
         Q _q;
         size_t _max_q_size;
         unordered_set<Node> _expansion;
         string _final_path;
         long long _run_time;
+        bool _is_found;
+
+        auto search() -> bool
+        {
+
+        }
     };
 }
