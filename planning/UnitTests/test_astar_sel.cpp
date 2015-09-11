21956465ade9cd9459357fe4f47d6ded69d627f4
moophy <MoophyAlan@gmail.com>
2015-09-12 11:09:37 +1200

    two more test cases for astar

--- planning/UnitTests/test_astar_sel.cpp
+++ planning/UnitTests/test_astar_sel.cpp
@@ -33,21 +33,59 @@ namespace UnitTests
 
         TEST_METHOD(astar_algorithm)
         {
-            unordered_set<State> blokeds{ { 3, 2 }, { 5, 4 } };
-            function<bool(Node)> validate = [&](Node const& n) {
-                auto is_not_bloked = none_of(blokeds.cbegin(), blokeds.cend(), [&](State s) {
-                    return s == n.state();
-                });
-                return is_not_bloked && n.state().is_with_in_grid({ 0, 0 }, { 39, 39 });
-            };
-            AStarSEL<ManhattanDistance<Node>, decltype(validate)> astar;
-            astar({ 0, 0 }, { 8, 12 }, validate);
+            {
+                unordered_set<State> blokeds{ { 3, 2 }, { 5, 4 } };
+                function<bool(Node)> validate = [&](Node const& n) {
+                    auto is_not_bloked = none_of(blokeds.cbegin(), blokeds.cend(), [&](State s) {
+                        return s == n.state();
+                    });
+                    return is_not_bloked && n.state().is_with_in_grid({ 0, 0 }, { 39, 39 });
+                };
+                AStarSEL<ManhattanDistance<Node>, decltype(validate)> astar;
+                astar({ 0, 0 }, { 8, 12 }, validate);
 
-            Assert::AreEqual(44u, astar.last_run.max_q_size);
-            Assert::AreEqual(32u, astar.last_run.expansions.size());
-            Assert::AreEqual(string{ "588885888838" }, astar.last_run.final_path);
-            Assert::IsTrue(100 < (int)astar.last_run.run_time && 1000 > (int)astar.last_run.run_time);
-            Assert::IsTrue(astar.last_run.is_found);
+                Assert::AreEqual(44u, astar.last_run.max_q_size);
+                Assert::AreEqual(32u, astar.last_run.expansions.size());
+                Assert::AreEqual(string{ "588885888838" }, astar.last_run.final_path);
+                Assert::IsTrue(100 < (int)astar.last_run.run_time && 1000 > (int)astar.last_run.run_time);
+                Assert::IsTrue(astar.last_run.is_found);
+            }
+
+            {
+                unordered_set<State> blokeds{ { 3, 3 }, { 5, 5 } };
+                function<bool(Node)> validate = [&](Node const& n) {
+                    auto is_not_bloked = none_of(blokeds.cbegin(), blokeds.cend(), [&](State s) {
+                        return s == n.state();
+                    });
+                    return is_not_bloked && n.state().is_with_in_grid({ 0, 0 }, { 39, 39 });
+                };
+                AStarSEL<ManhattanDistance<Node>, decltype(validate)> astar;
+                astar({ 0, 0 }, { 30, 30 }, validate);
+
+                Assert::AreEqual(119u, astar.last_run.max_q_size);
+                Assert::AreEqual(75u, astar.last_run.expansions.size());
+                Assert::AreEqual(string{ "8858887888888888888888888888888" }, astar.last_run.final_path);
+                Assert::IsTrue(100 < (int)astar.last_run.run_time && 5000 > (int)astar.last_run.run_time);
+                Assert::IsTrue(astar.last_run.is_found);
+            }
+
+            {//when goal is blocked, it's gonna be quite slow.
+                unordered_set<State> blokeds{ { 3, 3 }, { 5, 5 }, { 9, 9 } };
+                function<bool(Node)> validate = [&](Node const& n) {
+                    auto is_not_bloked = none_of(blokeds.cbegin(), blokeds.cend(), [&](State s) {
+                        return s == n.state();
+                    });
+                    return is_not_bloked && n.state().is_with_in_grid({ 0, 0 }, { 9, 9 });
+                };
+                AStarSEL<ManhattanDistance<Node>, decltype(validate)> astar;
+                astar({ 0, 0 }, { 9, 9 }, validate);
+
+                Assert::AreEqual(32u, astar.last_run.max_q_size);
+                Assert::AreEqual(97u, astar.last_run.expansions.size());
+                Assert::AreEqual(string{ }, astar.last_run.final_path);
+                Assert::IsTrue(100 < (int)astar.last_run.run_time && 5000 > (int)astar.last_run.run_time);
+                Assert::IsFalse(astar.last_run.is_found);
+            }
         }
     };
 }
