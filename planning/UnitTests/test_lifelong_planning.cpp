7f794b00ef315d3e646a875726132c3b51220780
moophy <MoophyAlan@gmail.com>
2015-09-15 18:05:40 +1200

    to decouple lp with a*, lp starts again from scratch.

--- planning/UnitTests/test_lifelong_planning.cpp
+++ planning/UnitTests/test_lifelong_planning.cpp
@@ -11,14 +11,47 @@ namespace UnitTests
     {
     public:
 
-        TEST_METHOD(LpState_struct)
+        TEST_METHOD(key)
         {
-            LpState ls = { { 42, 99 }, 66, 67 };
-            Assert::AreEqual(42, ls.state.y);
-            Assert::AreEqual(99, ls.state.x);
-            Assert::AreEqual(66, ls.g);
-            Assert::AreEqual(67, ls.r);
+            Key key{ 42, 99 };
+            Assert::AreEqual(42, key.first);
+            Assert::AreEqual(99, key.second);
         }
 
+        TEST_METHOD(coordinate)
+        {
+            Coordinate c{ 42, 99 };
+            Assert::AreEqual(42, c.x);
+            Assert::AreEqual(99, c.y);
+        }
+
+        TEST_METHOD(lp_manhattan)
+        {
+            Assert::AreEqual(39, LpManhattanDistance{ { 39, 39 } }({ 0, 0 }));
+            Assert::AreEqual(39, LpManhattanDistance{ { 38, 39 } }({ 0, 0 }));
+            Assert::AreEqual(38, LpManhattanDistance{ { 38, 38 } }({ 0, 0 }));
+        }
+
+        TEST_METHOD(lp_euclidean_distance)
+        {
+            Assert::AreEqual(100, LpEuclideanDistance{ { 60, 80 } }({ 0, 0 }));
+            Assert::AreEqual(50, LpEuclideanDistance{ { 30, 40 } }({ 0, 0 }));
+            Assert::AreEqual(14, LpEuclideanDistance{ { 10, 10 } }({ 0, 0 }));
+        }
+
+        TEST_METHOD(lp_state)
+        {
+            auto ls = LpState{ { 3, 4 }, 6, 7 };
+            Assert::AreEqual(3, ls.c.x);
+            Assert::AreEqual(4, ls.c.y);
+            Assert::AreEqual(6, ls.g);
+            Assert::AreEqual(7, ls.r);
+
+            Assert::IsTrue(Key{ 6, 6 } == ls.key(LpManhattanDistance{ { 39, 29 } }));
+            Assert::IsTrue(Key{ 6, 6 } == ls.key(LpManhattanDistance{ { 6, 7 } }));
+
+            Assert::IsTrue(Key{ 6, 6 } == ls.key(LpEuclideanDistance{ { 39, 29 } }));
+            Assert::IsTrue(Key{ 6, 6 } == ls.key(LpEuclideanDistance{ { 6, 7 } }));
+        }
     };
 }
--- planning/lib/lifelong_planning.hpp
+++ planning/lib/lifelong_planning.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "node.hpp"
-using namespace search;
 using std::make_pair;
 using std::min;
+using std::abs;
+using std::hypot;
 
 namespace search
 {
@@ -12,19 +12,52 @@ namespace search
     //
     namespace lp
     {
+        struct Key
+        {
+            int first, second;
+            friend auto operator== (Key l, Key r)
+            {
+                return l.first == r.first && l.second == r.second;
+            }
+        };
+
+        struct Coordinate
+        {
+            int x, y;
+            friend auto operator== (Coordinate l, Coordinate r)
+            {
+                return l.x == r.x && l.y == r.y;
+            }
+        };
+
         struct LpState
         {
-            State state;
+            Coordinate c;
             int g, r;
-            using Key = std::pair<int, int>;
 
-            //
-            //  Calculate Key(s)
-            //
             template<typename Hfunc>
             auto key(Hfunc h) const -> Key
             {
-                return make_pair(min(g, r + h(x, y), min(g, r)));
+                return{ min(g, r + h(c)), min(g, r) };
+            }
+        };
+
+        struct LpManhattanDistance
+        {
+            Coordinate goal;
+            auto operator()(Coordinate c) const -> int
+            {
+                return max(abs(goal.x - c.x), abs(goal.y - c.y));
+            }
+        };
+
+        struct LpEuclideanDistance
+        {
+            Coordinate goal;
+            auto operator()(Coordinate c) const -> int
+            {
+                auto result = hypot(abs(goal.x - c.x), abs(goal.y - c.y));
+                return static_cast<int>(round(result));
             }
         };
     }
