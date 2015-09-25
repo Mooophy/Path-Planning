a85087935af9d038c08b8f6e97053c20bd9d9492
moophy <MoophyAlan@gmail.com>
2015-09-26 03:14:27 +1200

    Initialization for km and q

--- planning/lib/d_star_final.hpp
+++ planning/lib/d_star_final.hpp
@@ -65,8 +65,8 @@ namespace search
                 start{ start },
                 goal{ goal },
                 hfunc{ HEURISTICS.at(heuristic) },
-                q{ [this](Cell l, Cell r) { return Key{ at(l) } < Key{ at(r) }; } },
-                km{ 0 }
+                km{ 0 },
+                q{ [this](Cell l, Cell r) { return Key{ at(l), km } < Key{ at(r), km }; } }
             {
                 mark_bad_cells(bad_cells);
                 mark_h_values_with(start);  //h value : start to current
@@ -78,8 +78,8 @@ namespace search
             Matrix matrix;
             Cell const start, goal;
             function<int(Cell, Cell)> const hfunc;
-            PriorityQueue < Cell, function<bool(Cell, Cell)> > q;
             int km;
+            PriorityQueue < Cell, function<bool(Cell, Cell)> > q;
             //
             //  statistics
             //
